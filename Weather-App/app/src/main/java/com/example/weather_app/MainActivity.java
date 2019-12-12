e8ffc0fc92135f4b291cebfb8bd6b90704ce03dd
Rushil Patel <ra8patel@edu.uwaterloo.ca>
2019-12-11 21:31:35 -0500

    receiving temp value

--- Weather-App/app/src/main/java/com/example/weather_app/MainActivity.java
+++ Weather-App/app/src/main/java/com/example/weather_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.weather_app;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +17,9 @@ import java.net.URL;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView temperatureView;
+
 
     //Async task - runs on background thread
     public class DownloadTask extends AsyncTask<String,Void,String> {
@@ -61,25 +65,28 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
 
-//            Log.i("JSON", s);
-//            System.out.println(s);
-
-
             try {
                 JSONObject jsonObject = new JSONObject(s);
-                String weatherInfo = jsonObject.getString("weather");
+
+                String weatherInfo = jsonObject.getString("main");
+                String temp = jsonObject.getJSONObject("main").getString("temp");
+
+
                 Log.i("Weather content", weatherInfo);
+                Log.i("temp", temp);
 
                 JSONArray arr = new JSONArray(weatherInfo);
 
-                for(int i=0; i < arr.length(); i++){
-                    JSONObject jsonPart = arr.getJSONObject(i);
+//                for (int i=0; i < arr.length(); i++) {
+//                    JSONObject jsonPart = arr.getJSONObject(i);
+//
+//                    Log.i("main",jsonPart.getString("humidity"));
+//                    Log.i("description",jsonPart.getString("temp"));
+//                }
 
-                    Log.i("main",jsonPart.getString("main"));
-                    Log.i("description",jsonPart.getString("description"));
-                }
+                temperatureView.setText(temp);
 
-            }catch(Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
 
@@ -91,7 +98,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        temperatureView = findViewById(R.id.temperatureView);
+
         DownloadTask task = new DownloadTask();
+//        task.execute("http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1");
         task.execute("http://api.openweathermap.org/data/2.5/weather?q=Toronto,Canada&units=metric&APPID=bf58619abfa156050b2f8b2cf2f40f6e");
+
     }
 }
